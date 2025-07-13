fc25643184db0cbfb99430df61cc09f27f90c270
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2025-07-13 19:20:14 +0100

    Renamed files with better naming

--- azure-pipelines/run-deployment-prep.ps1
+++ azure-pipelines/run-deployment-prep.ps1
@@ -1,4 +1,17 @@
+$pat = "jzX3ZYCiI1oMRuo6SA8IOCa9pq1Kmom2erp7eJsgFCfxg0CUO2tVJQQJ99BGACAAAAApUUOzAAASAZDO3GXk"
+$targetBranch = "release/deployment-september-2025-sale"
+$primaryReleaseTag = "sep-sale-25"
+$newUATVersionTag = "#Release-Sep25-v2"
+$newPRODVersionTag = "prd-20250714"
 
+$organization = "Next-Technology"
+$project = "Ecom.Sale"
+$boardColumnField = "WEF_C20432DD5A1E4B66931BC16E0AC05E8C_Kanban.Column"
+$repositories = @("ecm-sale-frontend")
+$boardColumnMapping = @{
+    "CI Testing" = "UAT Testing"
+    "Default" = "Deployed to UAT"
+}
 
 $uatDeploymentPrep = $true
 $prodDeploymentPrep = $false
@@ -9,7 +22,7 @@ if($uatDeploymentPrep)
 
     Write-Host "Step 2 - Started Tagging Tickets & Moving to UAT Board Column"
 
-    .\tag-tickets-and-move-to-uat.ps1 `
+    .\tag-and-move-tickets-across-board.ps1 `
         -organization $organization `
         -project $project `
         -pat $pat `
@@ -27,7 +40,7 @@ if($prodDeploymentPrep)
     Write-Host "Starting PROD Deployment Preparation"
 
     Write-Host "Step 1 - Started Tagging Tickets"
-    .\tag-tickets-and-move-to-uat.ps1 `
+    .\tag-and-move-tickets-across-board.ps1 `
         -organization $organization `
         -project $project `
         -pat $pat `
