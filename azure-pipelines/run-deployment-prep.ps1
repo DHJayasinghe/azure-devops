0901e45d637dff425143272f1a00cc9d1595bac0
Dhanuka Jayasinghe <hasitha2kandy@gmail.com>
2025-07-13 22:18:49 +0100

    Removed configs

--- azure-pipelines/run-deployment-prep.ps1
+++ azure-pipelines/run-deployment-prep.ps1
@@ -1,9 +1,21 @@
-$pat = "jzX3ZYCiI1oMRuo6SA8IOCa9pq1Kmom2erp7eJsgFCfxg0CUO2tVJQQJ99BGACAAAAApUUOzAAASAZDO3GXk"
-$targetBranch = "release/deployment-september-2025-sale"
-$primaryReleaseTag = "sep-sale-25"
+# Decide before execute - IMPORTANT
+$uatDeploymentPrep = $false
+$prodDeploymentPrep = $true
+
+# Change when expired - IMPORTANT
+$pat = "PAT_TOKEN"
+
+# Change during every release - IMPORTANT
 $newUATVersionTag = "#Release-Sep25-v2"
 $newPRODVersionTag = "prd-20250714"
+$prodDropNumber = "Drop 1" # Suggestion - Instead repeating "Sep '25 Sale -" prefix for the query folders can we just use "All" & "Drop 1"? Main folder already got this prefix
 
+# Change during every sale
+$targetBranch = "release/deployment-september-2025-sale" # Suggestion - Can we standardize this to use the same $primaryReleaseTag? And Term "deployment" = "release/"
+$primaryReleaseTag = "sep-sale-25"
+$releaseQueryFolderName = "Test Automation" # Sep '25  # Suggestion - Can we standardize this to use the same $primaryReleaseTag?
+
+# Constants
 $organization = "Next-Technology"
 $project = "Ecom.Sale"
 $boardColumnField = "WEF_C20432DD5A1E4B66931BC16E0AC05E8C_Kanban.Column"
@@ -12,15 +24,15 @@ $boardColumnMapping = @{
     "CI Testing" = "UAT Testing"
     "Default" = "Deployed to UAT"
 }
+$releaseQueryProjectName = "OnlineTech.Backlog"
+$releaseQueryFolderPath = "Shared Queries/Sale/Releases"
 
-$uatDeploymentPrep = $true
-$prodDeploymentPrep = $false
 
 if($uatDeploymentPrep)
 {
     Write-Host "Starting UAT Deployment Preparation"
 
-    Write-Host "Step 2 - Started Tagging Tickets & Moving to UAT Board Column"
+    Write-Host "Step 2 - Tagging for UAT Release & Moving across the Board"
 
     .\tag-and-move-tickets-across-board.ps1 `
         -organization $organization `
@@ -39,7 +51,7 @@ if($prodDeploymentPrep)
 {
     Write-Host "Starting PROD Deployment Preparation"
 
-    Write-Host "Step 1 - Started Tagging Tickets"
+    Write-Host "Step 1 - Tagging for Prod Release"
     .\tag-and-move-tickets-across-board.ps1 `
         -organization $organization `
         -project $project `
@@ -50,4 +62,18 @@ if($prodDeploymentPrep)
         -newVersionTag $newPRODVersionTag `
         -boardColumnField @{} `
         -repositories $repositories
+
+    Write-Host "Step 2 - Creating Board Queries"
+
+    .\create-board-query-for-release.ps1 `
+        -organization $organization `
+        -project $releaseQueryProjectName  `
+        -pat $pat `
+        -folderPath $releaseQueryFolderPath `
+        -targetFolderName $releaseQueryFolderName `
+        -tag $newPRODVersionTag `
+        -queriesToCreate @(
+            @{ Name = "All"; Mode = "Merge" }, 
+            @{ Name = $prodDropNumber; Mode = "Replace" }
+        )    
 }
